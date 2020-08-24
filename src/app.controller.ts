4cb6c27a14787110e4f426941ac5100a45dee94f
Nugine <nugine@foxmail.com>
2020-08-24 22:05:52 +0800

    添加配置模块

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,23 @@
 import { Controller, Get } from "@nestjs/common";
 import { AppService } from "./app.service";
+import { ConfigService } from "./config/config.service";
+import { Config } from "./config/config";
 
 @Controller()
 export class AppController {
-    constructor(private readonly appService: AppService) {}
+    constructor(
+        private readonly appService: AppService,
+        private readonly configService: ConfigService
+    ) {}
 
     @Get()
     getHello(): string {
         return this.appService.getHello();
+    }
+
+    // FIXME: remove this api in production
+    @Get("/test")
+    getConfig(): Config {
+        return this.configService.getConfig();
     }
 }
