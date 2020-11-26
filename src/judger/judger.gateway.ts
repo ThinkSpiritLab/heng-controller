ec7061088aebf31644684ada7b0f3bc44a731f4a
root <root@SSOJ-Test-JudgeServer.lan>
2020-11-26 11:47:03 +0800

    extract

--- src/judger/judger.gateway.ts
+++ src/judger/judger.gateway.ts
@@ -10,6 +10,7 @@ import {
 import { Server } from "ws";
 import WebSocket from "ws";
 import { RedisService } from "src/redis/redis.service";
+import { IncomingMessage } from "http";
 
 @WebSocketGateway({ path: "/judger/ws" })
 export class JudgerGateway
@@ -18,26 +19,35 @@ export class JudgerGateway
     server!: Server;
     constructor(private redisService: RedisService) {}
 
-    afterInit(server: Server) {
-        this.server.on("connection", async (s, r) => {
-            Logger.log(`Connect with req ${r.url}`, "WebSocketGateway");
-            if (r.url !== undefined) {
-                let url = new URL(r.url, "ws://ws.org");
-                let token = url.searchParams.get("token");
-                if (token !== null && await this.redisService.deleteToken(token)) {
-                    return;
-                } else {
-                    Logger.log(
-                        "Refused Connect as badtoken",
-                        "WebSocketGateway"
-                    );
-                    s.close();
-                }
+    async handleConnect(
+        connect: WebSocket,
+        req: IncomingMessage
+    ): Promise<void> {
+        Logger.log(`Connect with req ${req.url}`, "WebSocketGateway");
+        if (req.url !== undefined) {
+            let token = new URL(req.url, "ws://ws.org").searchParams.get(
+                "token"
+            );
+            if (
+                token !== null &&
+                (await this.redisService.deleteToken(token))
+            ) {
+                return;
             } else {
-                Logger.log("Refused Connect as no url", "WebSocketGateway");
-                s.close();
+                Logger.log(
+                    "Refused Connect because of badtoken",
+                    "WebSocketGateway"
+                );
+                connect.close();
             }
-        });
+        } else {
+            Logger.log("Refused Connect as no url", "WebSocketGateway");
+            connect.close();
+        }
+    }
+
+    afterInit(server: Server) {
+        this.server.on("connection", (c, r) => this.handleConnect(c, r));
         Logger.log(`Inited on ${this.server.address()}`, "WebSocketGateway");
     }
     handleDisconnect(@ConnectedSocket() client: WebSocket) {
