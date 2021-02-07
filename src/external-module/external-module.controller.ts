20063d196389507b7d214a01274449abd367a67b
rev-str <909363074@qq.com>
2021-02-07 09:44:07 +0800

    reqcat

--- src/external-module/external-module.controller.ts
+++ src/external-module/external-module.controller.ts
@@ -1,22 +1,23 @@
 import { Controller, Post } from '@nestjs/common';
 import { ExternalProtocol } from "heng-protocol";
-import { ReqPrc } from "./external-module.service"
+import { ExternalModuleService } from "./external-module.service"
 import { JudgeQueueService } from "SchedulerModule";
 import CreateJudgeRequest = ExternalProtocol.Post.CreateJudgeRequest
 @Controller('external-module')
 export class ExternalModuleController {
     constructor(
         private readonly scheduler: JudgeQueueService,
-        private readonly reqprc: ReqPrc,
+        private readonly externalmoduleService: ExternalModuleService,
     ){}
 
-    @Post("/get-req")
+    @Post("/v1/judges")
     async requestTransmit(request :CreateJudgeRequest): Promise <string> {//获得评测指令
         //处理评测指令
             //处理评测指令，返回taskid
-        const taskid = await this.reqprc.push(request)
+        const taskid = await this.externalmoduleService.ReqPrc(request)
         //发放评测指令
         const requestresult = await this.scheduler.push(taskid)
+        // 返回评测结果
         return requestresult
     }
 }
--- src/external-module/external-module.service.ts
+++ src/external-module/external-module.service.ts
@@ -1,4 +1,48 @@
 import { Injectable } from '@nestjs/common';
-
+import { timeStamp } from 'console';
+import { json } from 'express';
+import e = require('express');
+import { InternalProtocol } from "heng-protocol";
+import { ExternalProtocol } from "heng-protocol";
+import { RedisService } from  "src/redis/redis.service"
+import CreateJudgeRequest = ExternalProtocol.Post.CreateJudgeRequest
 @Injectable()
-export class ExternalModuleService {}
+export class ExternalModuleService {
+    constructor (
+        private readonly redisService: RedisService
+    ){}
+    
+    ReqCat(request: CreateJudgeRequest, cat: ExternalProtocol.Post.ExtendJudgeRequest): CreateJudgeRequest {
+        if (cat.judgeCallbackUrls == undefined ){
+            cat.judgeCallbackUrls = request.body.mainJudge.judgeCallbackUrls
+        }
+        let mainJudge: ExternalProtocol.Post.JudgeRequest = {
+            taskId: cat.taskId,
+            judge: cat.judge,
+            judgeCallbackUrls: cat.judgeCallbackUrls,
+        }
+        let Body : ExternalProtocol.Post.CreateJudgePayload = {
+            mainJudge: mainJudge
+        }
+        let req: CreateJudgeRequest = {
+            nonce: request.nonce,
+            timestamp: request.timestamp,
+            body:Body,
+            accesskey: request.accesskey,
+            signature: request.signature
+        }
+        return req;
+    }
+
+    async ReqPrc(request: CreateJudgeRequest): Promise<string> {
+        if (request.body.extra != undefined) {
+            request.body.extra.forEach(element => {
+                const req = this.ReqCat(request,element);
+                 
+            });
+        }
+        this.redisService.client.set(request.body.mainJudge.taskId,JSON.stringify(request))
+        return request.body.mainJudge.taskId
+    }
+    
+}
