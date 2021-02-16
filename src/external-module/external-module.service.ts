f3b074a3d1e345bea45cf32295ee6fa9eb29c523
rev-str <909363074@qq.com>
2021-02-16 08:41:23 +0800

    newcommit

--- src/external-module/external-module.service.ts
+++ src/external-module/external-module.service.ts
@@ -1,34 +1,38 @@
 import { Injectable } from '@nestjs/common';
 import { RedisService } from  "src/redis/redis.service"
-// import { JudgeQueueService } from "SchedulerModule";
 import { Logger } from "@nestjs/common"
-import * as InternalProtocol from "heng-protocol/Internal-protocol"
 import * as crypto from "crypto"
-import InHTTP = InternalProtocol.HTTP
 import { random } from 'lodash';
 import { AcquireTokenOutput, AcquireTokenRequest, ErrorInfo } from 'heng-protocol/Internal-protocol/http';
-import {} from "heng-protocol/external-protocol"
-import { CreateJudgeRequest } from 'heng-protocol/Internal-protocol/ws';
+import { CreateJudgeRequest, ExitArgs, ReportStatusRequest, ReportStatusResponse } from 'heng-protocol/Internal-protocol/ws'
 import { AppGateway } from 'src/app.gateway';
+import moment from 'moment'
+import { JudgerGateway } from 'src/judger/judger.gateway.ts'
+import { JudgerService } from 'src/judger/judger.service.ts'
+import { JudgeQueueService } from "SchedulerModule"
 @Injectable()
 export class ExternalModuleService {
     private readonly logger = new Logger('ExternalModuleService')
     constructor (
-       //  private readonly judgequeueService: JudgeQueueService,
+        private readonly judgequeueService: JudgeQueueService,
         private readonly redisService: RedisService,
-        private readonly gateway: AppGateway
+        private readonly gateway: JudgerGateway,
+        private readonly judgerservice: JudgerService,
     ){}
     
+
+
+    //评测机注册，建立ws连接
     async JudgeLogin(req: AcquireTokenRequest): Promise <ErrorInfo | AcquireTokenOutput> {
-        let errorinfo : InHTTP.ErrorInfo = {
+        let errorinfo : ErrorInfo = {
             code:1,
-            message:'error:名称冲突',
+            message:'名称与已有内容重复',
         } 
         if (await this.redisService.client.hexists('logon',req.name) == 1) 
             return errorinfo
         
         // 生成token并返回
-        let token = crypto
+        let token = await crypto
         .createHmac("sha256",'secretkey')
         .update(String(random()))
         .digest('hex')
@@ -39,11 +43,57 @@ export class ExternalModuleService {
         return tokenoutput
     }
 
-    
-    // debug
-    async createjudge(): Promise <void> {
-        let judgerequest : CreateJudgeRequest
-        this.gateway.emit()
+    // 发送评测任务并等待回调
+    async createjudge(req: CreateJudgeRequest): Promise <void> {
+        let taskid : number = parseInt(await this.redisService.client.get('Taskids'))+1 ?? 1
+        await this.redisService.client.set('Taskids',taskid)
+        this.redisService.client.hmset('Task',taskid,JSON.stringify(req))
+        console.log('发送id为: ${taskid} 的任务')
+        await this.judgequeueService.push(taskid)
+        //塞入队列后，等待评测机评测完毕后将结果回调
+        //Q:评测机直接调用url还是在这里调用呢？
+        //await this.clientgateway.emit('callbackurl',JSON.stringify(req))
+    }
+
+
+    //Exit，控制端要求下线
+    async Exit(
+        wsId: string,
+        {reason}: ExitArgs
+    ): Promise<void> {
+        await this.gateway.forceDisconnect(wsId,reason);
+        console.log('控制端要求下线')
+    }
+
+
+    async getLog(
+        wsId: string
+    ): Promise<string> {
+        console.log(' 读取 Id为 ${wsId} 的评测机的日志 ')
+       return await this.judgerservice.getlog(wsId)
+    }
+
+
+    async reportStatus(
+        reportstatus: ReportStatusRequest
+    ): Promise <ReportStatusResponse>{
+        console.log('报告状况')
+        try{
+        return await this.judgerservice.reportStatus(reportstatus) //调用api?
+        } catch (error){
+            let ret: ReportStatusResponse = {
+                type : "res",
+                seq: 3,
+                time: moment().format("YYYY-MM-DDTHH:mm:ssZ"),
+                body : {
+                    error: {
+                        code: 504,
+                        message: '等待超时'
+                    }
+                }
+            }
+            return ret
+        }
     }
 
 }
