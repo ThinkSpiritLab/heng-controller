bc7f3fe3a339f770c3e37cc11cfbbd5366b1d4d1
rev-str <909363074@qq.com>
2021-02-16 09:41:59 +0800

    externalmodule

--- src/external-module/external-module.controller.ts
+++ src/external-module/external-module.controller.ts
@@ -4,16 +4,14 @@ import { ExternalModuleService } from "./external-module.service";
 @Controller("external-module")
 export class ExternalModuleController {
     constructor(
-        private readonly externalmoduleService: ExternalModuleService,
-    ){}
-    
+        private readonly externalmoduleService: ExternalModuleService
+    ) {}
+
     @Post("v1/judgers/token")
-    async JudgeLogin(@Req() request:any): Promise <any> {
+    async JudgeLogin(@Req() request: any): Promise<any> {
         // check signature valid
         // if (valid == 0) return errorinfo
         const body: InternalProtocol.HTTP.AcquireTokenRequest = request.query;
         return this.externalmoduleService.JudgeLogin(body);
     }
-    
-    
 }
--- src/external-module/external-module.service.ts
+++ src/external-module/external-module.service.ts
@@ -1,54 +1,65 @@
 import { Injectable } from "@nestjs/common";
-import { RedisService } from  "src/redis/redis.service";
+import { RedisService } from "src/redis/redis.service";
 import { Logger } from "@nestjs/common";
 import * as crypto from "crypto";
 import { random } from "lodash";
-import { AcquireTokenOutput, AcquireTokenRequest, ErrorInfo } from "heng-protocol/internal-protocol/http";
-import { CreateJudgeRequest, ExitArgs, ReportStatusRequest, ReportStatusResponse } from "heng-protocol/internal-protocol/ws";
+import {
+    AcquireTokenOutput,
+    AcquireTokenRequest,
+    ErrorInfo
+} from "heng-protocol/internal-protocol/http";
+import {
+    CreateJudgeRequest,
+    ExitArgs,
+    ReportStatusRequest,
+    ReportStatusResponse
+} from "heng-protocol/internal-protocol/ws";
 import moment from "moment";
 import { JudgerGateway } from "src/judger/judger.gateway";
 import { JudgerService } from "src/judger/judger.service";
 import { JudgeQueueService } from "src/scheduler/judge-queue-service/judge-queue-service.service";
 @Injectable()
 export class ExternalModuleService {
-    private readonly logger = new Logger("ExternalModuleService")
-    constructor (
+    private readonly logger = new Logger("ExternalModuleService");
+    constructor(
         private readonly judgequeueService: JudgeQueueService,
         private readonly redisService: RedisService,
         private readonly gateway: JudgerGateway,
-        private readonly judgerservice: JudgerService,
-    ){}
-    
-
+        private readonly judgerservice: JudgerService
+    ) {}
 
     //评测机注册，建立ws连接
-    async JudgeLogin(req: AcquireTokenRequest): Promise <ErrorInfo | AcquireTokenOutput> {
-        const errorinfo : ErrorInfo = {
-            code:1,
-            message:"名称与已有内容重复",
-        }; 
-        if (await this.redisService.client.hexists("logon",req.name) == 1) 
+    async JudgeLogin(
+        req: AcquireTokenRequest
+    ): Promise<ErrorInfo | AcquireTokenOutput> {
+        const errorinfo: ErrorInfo = {
+            code: 1,
+            message: "名称与已有内容重复"
+        };
+        if ((await this.redisService.client.hexists("logon", req.name)) == 1)
             return errorinfo;
-        
+
         // 生成token并返回
         const token = await crypto
-            .createHmac("sha256","secretkey")
+            .createHmac("sha256", "secretkey")
             .update(String(random()))
             .digest("hex");
-        const tokenoutput : AcquireTokenOutput = {
+        const tokenoutput: AcquireTokenOutput = {
             token: token
         };
-        this.redisService.client.hmset("Onlinetoken",token,req.name);
+        this.redisService.client.hmset("Onlinetoken", token, req.name);
         return tokenoutput;
     }
 
     // 发送评测任务并等待回调
-    async createjudge(req: CreateJudgeRequest): Promise <void> {
-        if (await this.redisService.client.get("Taskids") == null)
-            await this.redisService.client.set("Taskids","1");
-        const taskid : number = parseInt(await this.redisService.client.get("Taskids") || "0")+1;
-        await this.redisService.client.set("Taskids",taskid);
-        this.redisService.client.hmset("Task",taskid,JSON.stringify(req));
+    async createjudge(req: CreateJudgeRequest): Promise<void> {
+        if ((await this.redisService.client.get("Taskids")) == null)
+            await this.redisService.client.set("Taskids", "1");
+        const taskid: number =
+            parseInt((await this.redisService.client.get("Taskids")) || "0") +
+            1;
+        await this.redisService.client.set("Taskids", taskid);
+        this.redisService.client.hmset("Task", taskid, JSON.stringify(req));
         console.log("发送id为: ${taskid} 的任务");
         await this.judgequeueService.push(String(taskid));
         //塞入队列后，等待评测机评测完毕后将结果回调
@@ -58,37 +69,29 @@ export class ExternalModuleService {
 
     //心跳不太会写
 
-
     //Exit，控制端要求下线
-    async Exit(
-        wsId: string,
-        {reason}: ExitArgs
-    ): Promise<void> {
-        await this.gateway.forceDisconnect(wsId,reason || "no reason");
+    async Exit(wsId: string, { reason }: ExitArgs): Promise<void> {
+        await this.gateway.forceDisconnect(wsId, reason || "no reason");
         console.log("控制端要求下线");
     }
 
-
-    async getLog(
-        wsId: string
-    ): Promise<string> {
+    async getLog(wsId: string): Promise<string> {
         console.log(" 读取 Id为 ${wsId} 的评测机的日志 ");
         return await this.judgerservice.getlog(wsId);
     }
 
-
     async reportStatus(
         reportstatus: ReportStatusRequest
-    ): Promise <ReportStatusResponse>{
+    ): Promise<ReportStatusResponse> {
         console.log("报告状况");
-        try{
+        try {
             return await this.judgerservice.reportStatus(reportstatus); //调用api?
-        } catch (error){
+        } catch (error) {
             const ret: ReportStatusResponse = {
-                type : "res",
+                type: "res",
                 seq: 3,
                 time: moment().format("YYYY-MM-DDTHH:mm:ssZ"),
-                body : {
+                body: {
                     error: {
                         code: 504,
                         message: "等待超时"
